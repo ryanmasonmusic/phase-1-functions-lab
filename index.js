f5202f780dc1afbf0e69a673327c4d3704a073e9
Ryan Mason <ryanmasonmusic@gmail.com>
2022-11-23 22:03:56 -0500

    commit tree a252021cc005d3ec0253adae4e0138e4a6a8b7f1

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue){
+    let hq = 42
+   return Math.abs(someValue - hq)
+}
+
+function distanceFromHqInFeet(someValue){
+    distanceFromHqInBlocks(someValue);
+    let hq = 42;
+    let block = (someValue - hq);
+    return Math.abs((block) * 264)
+}
+function distanceTravelledInFeet(starting, destination){
+    return Math.abs((destination - starting) * 264)
+}
+
+function calculatesFarePrice(starting, destination){
+    let distance = Math.abs((destination - starting) * 264)
+    if (distance <= 400) {
+    return 0
+} else if (distance <= 2000){
+    return ((distance - 400) * .02)
+} else if (distance <= 2500){
+    return 25
+} else if (distance >= 2500) {
+    return 'cannot travel that far'
+}
+}
